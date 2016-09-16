ac79f9546be0c07044f528f75650ea8343ff9dc0
Romeu Gomes <oh4ianon@hotmail.com>
2016-09-16 04:17:10 +0100

    Add valgrind.sh file.

--- cprojinit.sh
+++ cprojinit.sh
@@ -79,6 +79,29 @@ if type "pprof" > /dev/null ; then
 fi
 
 if type "valgrind" > /dev/null ; then
-	echo ""
+	echo "Creating $1/valgrind.sh"
+
+	cat << EOF >> $1/valgrind.sh
+#!/bin/sh
+
+if [ "\$#" -lt 2 ]; then
+  echo "Usage: ./valgrind.sh [tool] [program]\ntools: memcheck callgrind"
+  exit 1
+fi
+
+echo "Tool: \$1"
+
+if [ "\$1" == "memcheck" ]; then
+  shift
+  valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-fds=yes --track-origins=yes --show-mismatched-frees=yes \$@
+fi
+
+if [ "\$1" == "callgrind" ]; then
+  shift
+  valgrind --tool=callgrind \$@
+fi
+EOF
+
+	chmod +x $1/valgrind.sh
 fi
 
